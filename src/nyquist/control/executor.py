40c549d80df406658b6b6303ed429e6a61805d45
marcomiretti <marco.miretti@gmail.com>
2021-04-10 18:22:42 -0300

    :sparkles: add control script executor
    This executor will help the user crafting control scripts.

---
+++ src/nyquist/control/executor.py
@@ -0,0 +1,90 @@
+from abc import ABC, abstractmethod
+import asyncio
+import time
+
+
+class Experiment(ABC):
+    def __init__(self):
+        self._loop = asyncio.get_event_loop()
+        self._loop_period_s = 0.1
+        self._run_time_s = 2 * 60
+        self._before_loop_time_s = 0.5
+        self._after_loop_time_s = 0
+
+    def set_loop_frequency(self, frequency_hz):
+        """
+        Define the frequency of the control-loop [Hz].
+
+        :param frequency_hz: The frequency in hertz.
+        :type frequency_hz: float
+        """
+        self._loop_period_s = 1 / frequency_hz
+
+    def set_run_time(self, time_s):
+        """
+        Define the simulation run time [s]. This includes only the time spent
+        inside the control loop.
+
+        :param time_s: The time in seconds.
+        :type time_s: float
+        """
+        self._run_time_s = time_s
+
+    def set_before_loop_time(self, time_s):
+        """
+        Set the delay time between the :meth:`Experiment.before_the_loop` and
+        the start of the loop.
+
+        :param time_s: The time in seconds.
+        :type time_s: float
+        """
+        self._before_loop_time_s = time_s
+
+    def set_after_loop_time(self, time_s):
+        """
+        Set the delay time between end of the loop and
+        :meth:`Experiment.after_the_loop`.
+
+        :param time_s: The time in seconds.
+        :type time_s: float
+        """
+
+        self._after_loop_time_s = time_s
+
+    async def control_algorithm(self):
+        try:
+            self.before_the_loop()
+            await asyncio.sleep(self._before_loop_time_s)
+            while (time.monotonic() - self._start_ts < self._run_time_s):
+                self.in_the_loop()
+                await asyncio.sleep(self._loop_period_s)
+            await asyncio.sleep(self._after_loop_time_s)
+        finally:
+            self.after_the_loop()
+
+    @abstractmethod
+    def before_the_loop(self):
+        """The user should define this method. When :meth:`Experiment.run` is
+        called, this method will run first. Then it will wait for some time
+        that can be set with :meth:`Experiment.set_before_loop_time` and after
+        said delay, it will run the loop.
+        """
+
+    @abstractmethod
+    def in_the_loop(self):
+        """The user should define this method. When :meth:`Experiment.run` is
+        called, this method will run in a loop, after
+        :meth:`Experiment.before_the_loop`. This method will run in a loop
+        with a frequency established by :meth:`Experiment.set_loop_frequency`.
+        """
+
+    @abstractmethod
+    def after_the_loop(self):
+        """The user should define this method. When :meth:`Experiment.run` is
+        called, this method will run after the loop, and after a delay that
+        can be configured with :meth:`Experiment.set_after_loop_time`.
+        """
+
+    def run(self):
+        self._start_ts = time.monotonic()
+        self._loop.run_until_complete(self.control_algorithm())
